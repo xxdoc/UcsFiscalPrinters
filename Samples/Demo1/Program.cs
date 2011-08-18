a122771d1d3f188f117720013953d1a1034f8b78
Vladimir Vissoultchev <wqweto@gmail.com>
2011-08-18 14:14:09 +0300

    Initial implementation

---
+++ Samples/Demo1/Program.cs
@@ -0,0 +1,17 @@
+﻿using UcsFiscalPrinters;
+
+namespace Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDeviceProtocol fp = (IDeviceProtocol)new cZekaProtocol();
+            fp.Init("COM1");
+            fp.StartReceipt(UcsFiscalReceiptTypeEnum.ucsFscRetFiscal, "1", "Operator", "0");
+            fp.AddPLU("Product 1", 1.23, 5, 2);
+            fp.AddPayment(UcsFiscalPaymentTypeEnum.ucsFscPmtCash, "Cash", 5);
+            fp.EndReceipt();
+        }
+    }
+}
